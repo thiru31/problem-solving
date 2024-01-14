5468784604469fc2c4122428793928a3a5fec56b
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-14 18:42:43 +0530

    Add in-place array rotation code

---
+++ arr-rotation-b2f-Implace.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void rotation(vector<int> arr, int steps)
+{
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin()+2);
+    reverse(arr.begin()+2, arr.end());
+    for (auto &&i : arr)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+
+    int size;
+    cout << "Enter the size of array :";
+    cin >> size;
+    vector<int> arr(size);
+    cout << "Enter the array elements :";
+    for (int &ele : arr)
+    {
+        cin >> ele;
+    }
+    int steps;
+    cout << "Enter the Number of steps for rotation :";
+    cin >> steps;
+    steps = steps % arr.size();
+    rotation(arr, steps);
+}
