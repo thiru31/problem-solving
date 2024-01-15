b8f3f8ecce912665a05d3f6d77d60775defdcc88
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-15 16:41:49 +0530

    Add move-eve-index-front.cpp

---
+++ move-eve-index-front.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void moveindex(vector<int> &arr);
+
+int main()
+{
+    // vector<int> arr = {1, 2, 3, 4, 5, 6};
+    int size;
+    cout << "Enter the size of array :";
+    cin >> size;
+    vector<int> arr(size);
+    cout << "Enter the array elements :";
+    for (int &ele : arr)
+    {
+        cin >> ele;
+    }
+    moveindex(arr);
+}
+
+void moveindex(vector<int> &arr)
+{
+    int j = 0;
+    for (int i = 0; i < arr.size(); i += 2)
+
+    {
+        {
+            swap(arr[i], arr[j++]);
+        }
+    }
+    sort(arr.begin() + (arr.size() / 2), arr.end());
+    for (int &ele : arr)
+        cout << ele << " ";
+}
