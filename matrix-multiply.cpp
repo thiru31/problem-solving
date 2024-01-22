4447537171f470aee078a92c80542114b851ff4b
Thirumalai <thirumalaicse2020@gmail.com>
2024-01-23 00:03:36 +0530

    Update matrix multiplication code to accept user input for matrix dimensions

--- matrix-multiply.cpp
+++ matrix-multiply.cpp
@@ -6,10 +6,14 @@ int main()
 
 {
     int r1, c1, r2, c2;
-    r1 = 2;
-    c1 = 2;
-    r2 = 2;
-    c2 = 2;
+    cout << "Enter the number of rows for matrix 1: ";
+    cin >> r1;
+    cout << "Enter the number of columns for matrix 1: ";
+    cin >> c1;
+    cout << "Enter the number of rows for matrix 2: ";
+    cin >> r2;
+    cout << "Enter the number of columns for matrix 2: ";
+    cin >> c2;
     int arr1[r1][c1];
     cout << "Enter elements of array 1 : " << endl;
     for (size_t i = 0; i < r1; i++)
