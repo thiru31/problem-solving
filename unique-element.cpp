57f3c0505f9f707317909e2e09e48be80531f608
Thirumalai <thirumalaicse2020@gmail.com>
2024-03-02 00:06:44 +0530

    Remove test cases from unique-element.cpp

--- unique-element.cpp
+++ unique-element.cpp
@@ -43,25 +43,3 @@ int main()
     find_unique(arr);
 
 }
-
-//Test cases I have tested for the above code:
-
-
-    // vector<int> arr1 = {1, 2, 3, 4, 5};
-    // find_unique(arr1); // Expected output: (nothing)
-
-    // // Test Case 2: Single unique element
-    // vector<int> arr2 = {1, 2, 3, 4, 5, 2};
-    // find_unique(arr2); // Expected output: 1 3 4 5
-
-    // // Test Case 3: Multiple unique elements
-    // vector<int> arr3 = {1, 2, 3, 4, 5, 2, 3, 4};
-    // find_unique(arr3); // Expected output: 1 5
-
-    // // Test Case 4: Empty array
-    // vector<int> arr4 = {};
-    // find_unique(arr4); // Expected output: (nothing)
-
-    // // Test Case 5: Array with negative numbers
-    // vector<int> arr5 = {-1, 2, -3, 4, -5, 2, 3, 4};
-    // find_unique(arr5); // Expected output: -1 -3 -5 3
